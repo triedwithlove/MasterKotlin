aa967cefd946c3ebb2fc2df4ff5ff189c728f711
Karly KOUEKA <karlykoueka@hotmail.com>
2024-06-09 22:11:31 -0400

    Works on variables and Strings

---
+++ Section004_01Variables/src/Variables.kt
@@ -0,0 +1,13 @@
+/**
+ * Karly KOUEKA
+ * copyright 2024
+ */
+
+fun main(args: Array<String>) {
+    var str = "(\\(\\";
+    println(str);
+    str = "(-.-)";
+    println(str);
+    str = "o_(\")(\")";
+    println(str);
+}
