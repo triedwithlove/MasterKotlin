397c8e5546b2afc9fda2ff3729c2d896060a2610
Karly KOUEKA <karlykoueka@hotmail.com>
2024-06-22 21:00:33 -0400

    Using String Template
    On a simple example we demonstrate how to use String template

---
+++ Section004_03StringTemplate/src/StringTemplate.kt
@@ -0,0 +1,12 @@
+/**
+ * Karly KOUEKA
+ * copyright 2024
+ */
+
+fun main(args: Array<String>) {
+    val client = "Mary";
+    val product = 3;
+    val price = 30;
+
+    println("Client $client needs to pay ${product * price} for her products");
+}
